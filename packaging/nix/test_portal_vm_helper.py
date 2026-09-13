6084ebfacf45ca42c759a9773ebfc8c93afacffd
Realm builder <vadim.evard@gmail.com>
2026-09-13 05:09:29 +0000

    test: reject expired portal request before main loop

--- packaging/nix/test_portal_vm_helper.py
+++ packaging/nix/test_portal_vm_helper.py
@@ -1,6 +1,11 @@
 import unittest
 
-from portal_vm_helper import frame_evidence, request_path, single_stream_node
+from portal_vm_helper import (
+    PortalClient,
+    frame_evidence,
+    request_path,
+    single_stream_node,
+)
 
 
 class PortalVmHelperContract(unittest.TestCase):
@@ -35,6 +40,77 @@ class PortalVmHelperContract(unittest.TestCase):
                 ValueError
             ):
                 frame_evidence(41, size, width, height)
+
+    def test_request_timeout_during_sync_call_never_enters_dead_loop(self):
+        expected = request_path(":1.42", "realm_create")
+
+        class Reply:
+            def unpack(self):
+                return (expected,)
+
+        class Connection:
+            def get_unique_name(self):
+                return ":1.42"
+
+            def signal_subscribe(self, *_args):
+                return 7
+
+            def signal_unsubscribe(self, subscription):
+                self.unsubscribed = subscription
+
+            def call_sync(self, *_args):
+                return Reply()
+
+        class Loop:
+            def quit(self):
+                pass
+
+            def run(self):
+                raise AssertionError("expired request entered a main loop")
+
+        class VariantType:
+            @staticmethod
+            def new(value):
+                return value
+
+        class GLib:
+            SOURCE_REMOVE = False
+
+            @staticmethod
+            def MainLoop():
+                return Loop()
+
+            @staticmethod
+            def timeout_add(_timeout_ms, callback):
+                callback()
+                return 9
+
+            @staticmethod
+            def source_remove(_source):
+                raise AssertionError("expired timeout source was removed twice")
+
+        GLib.VariantType = VariantType
+
+        class DBusCallFlags:
+            NONE = 0
+
+        class DBusSignalFlags:
+            NONE = 0
+
+        class Gio:
+            pass
+
+        Gio.DBusCallFlags = DBusCallFlags
+        Gio.DBusSignalFlags = DBusSignalFlags
+
+        portal = PortalClient(Connection(), Gio, GLib)
+        with self.assertRaisesRegex(TimeoutError, "CreateSession"):
+            portal.begin_request(
+                "org.freedesktop.portal.ScreenCast",
+                "CreateSession",
+                None,
+                "realm_create",
+            )
 
 
 if __name__ == "__main__":
